e15fc7a67181f065814f30db9f4502c07bf8f264
ahamed <sabbir35-2988@diu.edu.bd>
2024-07-14 02:58:34 +0600

    Added student attributes for checking

--- src/main/java/com/sabbir/model/Student.java
+++ src/main/java/com/sabbir/model/Student.java
@@ -1,4 +1,16 @@
 package com.sabbir.model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Student {
+    private String name;
+    private String email;
+    private String password;
+    private Integer age;
+    private String DateOfBirth;
 }
