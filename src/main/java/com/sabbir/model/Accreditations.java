f8f296094aafee8ce29306b75c699d505abfa760
ahamed <sabbir35-2988@diu.edu.bd>
2024-07-15 17:33:33 +0600

    University Attributes Added Two new model added (Accreditations, Subjects)

---
+++ src/main/java/com/sabbir/model/Accreditations.java
@@ -0,0 +1,13 @@
+package com.sabbir.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Accreditations {
+    private Integer accreditationsId;
+    private String accreditationsName;
+}
---
+++ src/main/java/com/sabbir/model/Subjects.java
@@ -0,0 +1,13 @@
+package com.sabbir.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Subjects {
+    private Integer subjectId;
+    private String subjectName;
+}
