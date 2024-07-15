45d0be67f2dd92a21553c411f4b05d0717247428
ahamed <sabbir35-2988@diu.edu.bd>
2024-07-15 15:22:40 +0600

    Chat Attributes Added

--- src/main/java/com/sabbir/model/Chat.java
+++ src/main/java/com/sabbir/model/Chat.java
@@ -1,4 +1,15 @@
 package com.sabbir.model;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Chat {
+    private Integer ChatId;
+    private Integer UniversityID;
+    private Integer StudentID;
 }
